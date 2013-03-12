3ced6d1359c3aac87a24a555c4b2f245b8f9cf3a
flop <flop@steelyeye.net>
2013-03-13 02:44:26 +0300

    Initial Commit

---
+++ ReColoredSprite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReColoredSprite
+//
+//  Created by Flop on 13/03/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
